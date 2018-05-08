5e0369c52c85d6355d3a98c11d81afc4c235ebca
Jay <quantong_xu@163.com>
2018-05-09 00:07:22 +0800

    seckill

---
+++ src/main/java/org/seckill/exception/SeckillCloseException.java
@@ -0,0 +1,15 @@
+package org.seckill.exception;
+
+/**
+ * Created by jay-xqt on 2018/5/8.
+ */
+public class SeckillCloseException extends SeckillException{
+
+    public SeckillCloseException(String message) {
+        super(message);
+    }
+
+    public SeckillCloseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
